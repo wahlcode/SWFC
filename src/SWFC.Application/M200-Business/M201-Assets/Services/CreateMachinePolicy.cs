82081109050c4011c270e7f4c374171b92e3adb7
Stephan <Stephan.wahl.86@icloud.com>
2026-03-27 12:07:31 +0100

    [M802.02] architecture enforcement and security decoupling

---
+++ src/SWFC.Application/M200-Business/M201-Assets/Services/CreateMachinePolicy.cs
@@ -0,0 +1,12 @@
+using SWFC.Application.M200_Business.M201_Assets.Commands;
+using SWFC.Application.M800_Security.M802_ApplicationSecurity.Authorization;
+
+namespace SWFC.Application.M200_Business.M201_Assets.Services;
+
+public sealed class CreateMachinePolicy : IAuthorizationPolicy<CreateMachineCommand>
+{
+    public AuthorizationRequirement GetRequirement(CreateMachineCommand request)
+    {
+        return new AuthorizationRequirement(new[] { "machine.create" });
+    }
+}
